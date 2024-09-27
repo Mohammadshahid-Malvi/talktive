58413a2514dcccf5046695992e8baa8a7f6a3745
Mohammadshahid Malvi <mohmmadmalvi@gmail.com>
2024-09-28 01:35:15 +0530

    Episode-4 Routing & Request handlers

--- homework.md
+++ homework.md
@@ -1,6 +1,14 @@
 Episode 3 :- Creating our express server
 
-- Create a repository :- git init
+- Create a repository
+    - git init :- initialize the git.
+    - git add README.md :- add files to git
+    - git commit -m "Episode-3 created a express server" :- commit code
+        Push code to github repository in github.com
+    - git remote add origin https://github.com/Mohammadshahid-Malvi/talktive.git
+    - git branch -M main :- creating a branch
+    - git push -u origin main :- push code to main branch
+
 - Initialize the repository
 - node_module, package.json, package-lock.json
 - Install express
@@ -11,3 +19,17 @@ Episode 3 :- Creating our express server
 - Different between caret & tilda
 - what are dependency
 - what is use of -g while npm install
+
+Episode 04 :- Routing and Request Handlers
+
+- initialize git
+- create a remote repo on github
+- push all code to remote origin
+- play with all route /hello, /hello/2
+- Order of the route matters.
+- Install postman app and make a workplace/collections -> test api call.
+- Write logic to handle GET | POST | PUT-PATCH | DELETE Api calls and test on POSTMAN
+- Explore routing and use of ?, *, + in route 
+- user of regex in routes /a/, /.*lastshouldcomethisonly$/
+- Reading the query params in the routes
+- Reading the dynamic route
--- src/app.js
+++ src/app.js
@@ -3,14 +3,36 @@ const express = require('express');
 // Creating a new Express.js Application
 const app = express();
 
-// Handle any request which are coming in port 3000
-app.use("/test",(req,res)=>{    //this fn is know as request handler.    
-    res.send('Hello, we are listening');
+// if url '/ab+c' so you can add as much character b only before c as you can.
+// if url '/ab*cd' so you can add as much character between ab & cd.
+// if url '/ab?c' so you can ac/abc : because b is optional over here...
+// if use regex like /.*fly$/ means at end fly must present.
+
+app.get("/user/:userId/:firstName/:lastName",(req,res)=>{
+    console.log(req.query);     //to get data from postman url like "?userId?userName"
+    console.log(req.params);    //to get data from here only also pass original data to postman url.
+    res.send({firstName:"Mohammadshahid",lastName:"Malvi"});
 });
 
-app.use("/hellos",(req,res)=>{
-    res.send('hellow, hallaw!')
+// Handle any request which are coming in port 3000
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Mohammadshahid",lastName:"Malvi"});
 });
+
+app.post("/user",(req,res)=>{
+    //Saving data to db
+    res.send("Save data to Database!");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send('USer Deleted Successfully !');
+});
+
+// app.use("/test",(req, res)=>{   //this fn is know as request handler.
+//     res.send("Dashboard...");
+// });
+
+
 
 // Creating web server on 3000 port and it is listening
 app.listen(3000, () => {
